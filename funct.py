3683c4a537514cb3f5088435b19f335560b0af90
rus_is <kniggaaaa@mail.ru>
2021-02-10 14:37:57 +0300

    funct( +load lvl)

--- funct.py
+++ funct.py
@@ -20,3 +20,16 @@ def load_image(name):
 def terminate():
     pygame.quit()
     sys.exit()
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
